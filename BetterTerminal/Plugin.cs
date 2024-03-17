74dec6f50bceaca67fac0e81925929706eb879fb
Jann Stute <jann.stute@protonmail.com>
2024-03-17 20:02:04 +0100

    Customize Template

---
+++ BetterTerminal/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Computerdores;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance { get; set; }
+
+    public static ManualLogSource Log => Instance.Logger;
+
+    private readonly Harmony _harmony = new(PluginInfo.PLUGIN_GUID);
+
+    public Plugin()
+    {
+        Instance = this;
+    }
+
+    private void Awake()
+    {
+
+        Log.LogInfo("BetterTerminal Awake()");
+    }
+}
