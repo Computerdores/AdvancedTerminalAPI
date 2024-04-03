cd4955b3a73ce67757180ba20e934e1ee5f469c9
Jann Stute <jann.stute@protonmail.com>
2024-04-03 13:44:36 +0200

    feat(InputFieldDriver)!: add OnInputChange Event

--- AdvancedTerminalAPI/InputFieldDriver.cs
+++ AdvancedTerminalAPI/InputFieldDriver.cs
@@ -16,6 +16,10 @@ public class InputFieldDriver {
 
     // <---- Public API ----> //
 
+    /// <summary>
+    /// Can be used to get or set the current Input by the Player.
+    /// (Set is shown in the terminal)
+    /// </summary>
     // ReSharper disable once MemberCanBePrivate.Global
     public string Input {
         get => _input;
@@ -25,13 +29,28 @@ public class InputFieldDriver {
         }
     }
     
+    /// <summary>
+    /// The Terminal being driven by this Driver
+    /// </summary>
     public Terminal VanillaTerminal { get; }
 
     public delegate void EnterTerminalEvent(bool firstTime);
-    public delegate void SubmitEvent(string text);
+    public delegate void StringEvent(string text);
 
+    /// <summary>
+    /// Triggered whenever the player enters the Terminal
+    /// </summary>
     public event EnterTerminalEvent OnEnterTerminal;
-    public event SubmitEvent OnSubmit;
+    
+    /// <summary>
+    /// Triggered whenever the Player Submits what they typed in the Terminal
+    /// </summary>
+    public event StringEvent OnSubmit;
+    
+    /// <summary>
+    /// Triggered whenever the Player types in the Terminal
+    /// </summary>
+    public event StringEvent OnInputChange;
 
     public InputFieldDriver(Terminal __instance) {
         // Init variables
@@ -73,6 +92,7 @@ public class InputFieldDriver {
             Input = newText.Substring(_displayedText.Length, newInputLength);
         }
         _renderToInputField();
+        OnInputChange?.Invoke(Input);
     }
 
     private void OnInputFieldSubmitHandler(string text) {
