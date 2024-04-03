8dc618c9dc97fc9ecd47324dce2bf36f52b2ebc1
Jann Stute <jann.stute@protonmail.com>
2024-04-03 18:36:16 +0200

    doc(ITerminal): finish documentation

--- AdvancedTerminalAPI/ITerminal.cs
+++ AdvancedTerminalAPI/ITerminal.cs
@@ -1,12 +1,25 @@
 ﻿namespace Computerdores; 
 
 public interface ITerminal {
-    public delegate string ArgumentPredictor(string partialArguments);
-    public delegate string DisplayTextSupplier(string finalArguments);
-    
+    /// <summary>
+    /// This will be called once per level entry.
+    /// </summary>
+    /// <param name="driver">The InputFieldDriver for the current level.</param>
     public void RegisterDriver(InputFieldDriver driver);
+    
+    /// <summary>
+    /// Should return the InputFieldDriver for the current level.
+    /// </summary>
     public InputFieldDriver GetDriver();
 
+    /// <summary>
+    /// Add a command to the Terminal.
+    /// </summary>
     public void AddCommand(ICommand command);
+    
+    /// <summary>
+    /// This should copy all commands to the given ITerminal.
+    /// Usually that means calling AddCommand for every command that has been added via AddCommand.
+    /// </summary>
     public void CopyCommandsTo(ITerminal terminal);
 }
