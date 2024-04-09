744459f2ab0a7a2cffb843ad6de0fb88ce5acc58
Jann Stute <jann.stute@protonmail.com>
2024-04-09 23:25:46 +0200

    chore: fix script for running without mod

--- local/run_noAPI.ps1
+++ local/run_noAPI.ps1
@@ -1,3 +1,3 @@
 ﻿# Make sure that the environment variable ProfileName is set to the name of the Thunderstore profile you want to use
-Copy-Item -Path "$Env:appdata\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\$Env:ProfileName\BepInEx\plugins\AdvancedTerminalAPI.dll"
+Remove-Item -Path "$Env:appdata\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\$Env:ProfileName\BepInEx\plugins\AdvancedTerminalAPI.dll"
 &"C:/Program Files (x86)/Steam/steam.exe" -applaunch 1966720 --doorstop-enable true --doorstop-target "$Env:appdata\Thunderstore Mod Manager\DataFolder\LethalCompany\profiles\$Env:ProfileName\BepInEx\core\BepInEx.Preloader.dll"
