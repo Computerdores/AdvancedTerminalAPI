f13344a75e26cd207b9d842e9b747494b8b41309
Jann Stute <jann.stute@protonmail.com>
2024-04-10 12:42:38 +0200

    feat(ViewCommand): now implements IAliasable

--- AdvancedTerminalAPI/Vanillin/Commands/ViewCommand.cs
+++ AdvancedTerminalAPI/Vanillin/Commands/ViewCommand.cs
@@ -4,29 +4,23 @@ using HarmonyLib;
 
 namespace Computerdores.Vanillin.Commands; 
 
-public class ViewCommand : ICommand, IPredictable {
+public class ViewCommand : ICommand, IAliasable {
     public string GetName() => "view";
-
-    public string PredictInput(string partialInput) {
-        throw new System.NotImplementedException(); // TODO
-    }
 
     /// <summary>
     /// For the vanilla implementation, see: <see cref="Terminal.LoadNewNode"/>.
     /// </summary>
     public CommandResult Execute(string input, ITerminal terminal) {
-        ViewThingCommand cmd = FromPlayerInput(terminal.GetDriver().VanillaTerminal, input);
-        return cmd?.Execute(input.Split(' ').Skip(1).Join(delimiter: " "), terminal) ?? CommandResult.GENERIC_ERROR;
+        string[] words = input.Split(' ');
+        var cmd = new ViewThingCommand(words[0]);
+        return cmd.Execute(words.Skip(1).Join(delimiter: " "), terminal);
     }
 
     public object Clone() => new ViewCommand();
 
-    public static IEnumerable<ICommand> GetAll(ITerminal term) {
+    public IEnumerable<ICommand> GetAll(ITerminal term) {
         return from noun in Util.FindKeyword(term, "view").compatibleNouns 
             where noun.noun.defaultVerb != null
             select new ViewThingCommand(noun.noun.word);
     }
-
-    private static ViewThingCommand FromPlayerInput(Terminal term, string input) =>
-        new(Util.FindKeyword(term, "view").FindNoun(input).noun.word);
 }
