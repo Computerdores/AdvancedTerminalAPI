80709ec4956d7516db52b7f102eeb06307eb66ed
Jann Stute <jann.stute@protonmail.com>
2024-04-19 23:04:21 +0200

    feat(InfoCommand): Implement IPredictable

--- AdvancedTerminalAPI/AdvancedTerminalAPI/Vanillin/Commands/InfoCommand.cs
+++ AdvancedTerminalAPI/AdvancedTerminalAPI/Vanillin/Commands/InfoCommand.cs
@@ -4,8 +4,16 @@ using HarmonyLib;
 
 namespace Computerdores.AdvancedTerminalAPI.Vanillin.Commands; 
 
-public class InfoCommand : ICommand, IAliasable {
+public class InfoCommand : ICommand, IAliasable, IPredictable {
     public string GetName() => "info";
+
+    public string PredictInput(string partialInput, ITerminal terminal) {
+        TerminalKeyword kw = Util.FindKeyword(terminal.GetDriver().VanillaTerminal, "info");
+        if (kw.compatibleNouns.VanillaStringMatch(partialInput, cn => cn.noun.word)?.
+                noun is { } a)
+            return a.word;
+        return partialInput;
+    }
 
     public CommandResult Execute(string input, ITerminal terminal) {
         string[] words = input.Split(' '); 
